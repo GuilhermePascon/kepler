b8d6592454a6b4ede5157ac0a3d5491629903bc9
Sinuca <guilherme@awarelog.com>
2024-01-18 15:09:31 -0300

    ready for presentation

--- src/App.js
+++ src/App.js
@@ -25,7 +25,7 @@ function Map() {
   const dispatch = useDispatch();
   const { data } = useSwr("covid", async () => {
     const response = await fetch(
-      "https://raw.githubusercontent.com/GuilhermePascon/misc/master/hack_prod_bottom_fields.json?token=GHSAT0AAAAAACMSP2P4GUKEOLHEKBRQBFZAZNIOUXQ"
+      "https://raw.githubusercontent.com/GuilhermePascon/misc/master/hack_prod_bottom.json?token=GHSAT0AAAAAACMSP2P57QMCASZTDB2TXL34ZNJOU2Q"
     );
     const data = await response.json();
     return data;
@@ -33,7 +33,27 @@ function Map() {
 
   const sampleConfig = {
     "visState": {
-      "filters": [],
+      "filters": [
+        {
+          "dataId": [
+            "map"
+          ],
+          "id": "axxr48abbl",
+          "name": [
+            "day"
+          ],
+          "type": "timeRange",
+          "value": [
+            1646215905000,
+            1646215905001
+          ],
+          "enlarged": true,
+          "plotType": "histogram",
+          "animationWindow": "incremental",
+          "yAxis": null,
+          "speed": 1
+        }
+      ],
       "layers": [
         {
           "id": "1icv34x",
@@ -238,7 +258,7 @@ function Map() {
               "lat1": "destinationLat",
               "lng1": "destinationLng"
             },
-            "isVisible": true,
+            "isVisible": false,
             "visConfig": {
               "opacity": 0.8,
               "thickness": 2,
@@ -260,9 +280,9 @@ function Map() {
                 10
               ],
               "targetColor": [
-                55,
-                29,
-                168
+                201,
+                0,
+                0
               ]
             },
             "hidden": false,
@@ -289,6 +309,298 @@ function Map() {
             "colorScale": "quantile",
             "sizeField": null,
             "sizeScale": "linear"
+          }
+        },
+        {
+          "id": "xvm7lj3",
+          "type": "grid",
+          "config": {
+            "dataId": "map",
+            "label": "Coleta 3D",
+            "color": [
+              241,
+              92,
+              23
+            ],
+            "highlightColor": [
+              252,
+              242,
+              26,
+              255
+            ],
+            "columns": {
+              "lat": "originLat",
+              "lng": "originLng"
+            },
+            "isVisible": false,
+            "visConfig": {
+              "opacity": 0.8,
+              "worldUnitSize": 64.0244,
+              "colorRange": {
+                "name": "Global Warming",
+                "type": "sequential",
+                "category": "Uber",
+                "colors": [
+                  "#5A1846",
+                  "#900C3F",
+                  "#C70039",
+                  "#E3611C",
+                  "#F1920E",
+                  "#FFC300"
+                ]
+              },
+              "coverage": 1,
+              "sizeRange": [
+                0,
+                500
+              ],
+              "percentile": [
+                0,
+                100
+              ],
+              "elevationPercentile": [
+                0,
+                100
+              ],
+              "elevationScale": 100,
+              "enableElevationZoomFactor": true,
+              "colorAggregation": "count",
+              "sizeAggregation": "count",
+              "enable3d": true
+            },
+            "hidden": false,
+            "textLabel": [
+              {
+                "field": null,
+                "color": [
+                  255,
+                  255,
+                  255
+                ],
+                "size": 18,
+                "offset": [
+                  0,
+                  0
+                ],
+                "anchor": "start",
+                "alignment": "center"
+              }
+            ]
+          },
+          "visualChannels": {
+            "colorField": null,
+            "colorScale": "quantile",
+            "sizeField": null,
+            "sizeScale": "linear"
+          }
+        },
+        {
+          "id": "mlkdloi",
+          "type": "grid",
+          "config": {
+            "dataId": "map",
+            "label": "Entrega 3D",
+            "color": [
+              136,
+              87,
+              44
+            ],
+            "highlightColor": [
+              252,
+              242,
+              26,
+              255
+            ],
+            "columns": {
+              "lat": "destinationLat",
+              "lng": "destinationLng"
+            },
+            "isVisible": false,
+            "visConfig": {
+              "opacity": 0.8,
+              "worldUnitSize": 51.8293,
+              "colorRange": {
+                "name": "ColorBrewer YlGnBu-6",
+                "type": "sequential",
+                "category": "ColorBrewer",
+                "colors": [
+                  "#ffffcc",
+                  "#c7e9b4",
+                  "#7fcdbb",
+                  "#41b6c4",
+                  "#2c7fb8",
+                  "#253494"
+                ]
+              },
+              "coverage": 1,
+              "sizeRange": [
+                0,
+                500
+              ],
+              "percentile": [
+                0,
+                100
+              ],
+              "elevationPercentile": [
+                0,
+                100
+              ],
+              "elevationScale": 100,
+              "enableElevationZoomFactor": true,
+              "colorAggregation": "count",
+              "sizeAggregation": "count",
+              "enable3d": true
+            },
+            "hidden": false,
+            "textLabel": [
+              {
+                "field": null,
+                "color": [
+                  255,
+                  255,
+                  255
+                ],
+                "size": 18,
+                "offset": [
+                  0,
+                  0
+                ],
+                "anchor": "start",
+                "alignment": "center"
+              }
+            ]
+          },
+          "visualChannels": {
+            "colorField": null,
+            "colorScale": "quantile",
+            "sizeField": null,
+            "sizeScale": "linear"
+          }
+        },
+        {
+          "id": "bn7xfb",
+          "type": "heatmap",
+          "config": {
+            "dataId": "map",
+            "label": "Heatmap Coleta",
+            "color": [
+              23,
+              184,
+              190
+            ],
+            "highlightColor": [
+              252,
+              242,
+              26,
+              255
+            ],
+            "columns": {
+              "lat": "originLat",
+              "lng": "originLng"
+            },
+            "isVisible": false,
+            "visConfig": {
+              "opacity": 0.8,
+              "colorRange": {
+                "name": "Global Warming",
+                "type": "sequential",
+                "category": "Uber",
+                "colors": [
+                  "#5A1846",
+                  "#900C3F",
+                  "#C70039",
+                  "#E3611C",
+                  "#F1920E",
+                  "#FFC300"
+                ]
+              },
+              "radius": 28.7
+            },
+            "hidden": false,
+            "textLabel": [
+              {
+                "field": null,
+                "color": [
+                  255,
+                  255,
+                  255
+                ],
+                "size": 18,
+                "offset": [
+                  0,
+                  0
+                ],
+                "anchor": "start",
+                "alignment": "center"
+              }
+            ]
+          },
+          "visualChannels": {
+            "weightField": null,
+            "weightScale": "linear"
+          }
+        },
+        {
+          "id": "0haphcj",
+          "type": "heatmap",
+          "config": {
+            "dataId": "map",
+            "label": "Heatmap Entrega",
+            "color": [
+              23,
+              184,
+              190
+            ],
+            "highlightColor": [
+              252,
+              242,
+              26,
+              255
+            ],
+            "columns": {
+              "lat": "destinationLat",
+              "lng": "destinationLng"
+            },
+            "isVisible": false,
+            "visConfig": {
+              "opacity": 0.8,
+              "colorRange": {
+                "name": "Uber Viz Diverging 1.5",
+                "type": "diverging",
+                "category": "Uber",
+                "colors": [
+                  "#00939C",
+                  "#5DBABF",
+                  "#BAE1E2",
+                  "#F8C0AA",
+                  "#DD7755",
+                  "#C22E00"
+                ]
+              },
+              "radius": 22
+            },
+            "hidden": false,
+            "textLabel": [
+              {
+                "field": null,
+                "color": [
+                  255,
+                  255,
+                  255
+                ],
+                "size": 18,
+                "offset": [
+                  0,
+                  0
+                ],
+                "anchor": "start",
+                "alignment": "center"
+              }
+            ]
+          },
+          "visualChannels": {
+            "weightField": null,
+            "weightScale": "linear"
           }
         }
       ],
@@ -320,7 +632,7 @@ function Map() {
           },
           "compareMode": false,
           "compareType": "absolute",
-          "enabled": true
+          "enabled": false
         },
         "brush": {
           "size": 0.5,
@@ -341,25 +653,25 @@ function Map() {
       }
     },
     "mapState": {
-      "bearing": 0,
-      "dragRotate": false,
-      "latitude": -14.149297507439197,
-      "longitude": -56.29476464297802,
-      "pitch": 0,
-      "zoom": 3.39685739320786,
+      "bearing": -30.473198826896578,
+      "dragRotate": true,
+      "latitude": -23.55816517896834,
+      "longitude": -48.81080498847461,
+      "pitch": 32.760041660327396,
+      "zoom": 4.81341166969599,
       "isSplit": false
     },
     "mapStyle": {
-      "styleType": "muted",
+      "styleType": "dark",
       "topLayerGroups": {},
       "visibleLayerGroups": {
         "label": true,
         "road": true,
-        "border": false,
+        "border": true,
         "building": true,
         "water": true,
         "land": true,
-        "3d building": false
+        "3d building": true
       },
       "threeDBuildingColor": [
         224.4071295378559,
@@ -368,7 +680,7 @@ function Map() {
       ],
       "mapStyles": {}
     }
-  };
+  }
 
   React.useEffect(() => {
     if (data) {
